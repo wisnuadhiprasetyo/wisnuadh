e9787f1acc87d8ac82180752df8d1b4f1c669656
wisnuadhiprasetyo <48474136+wisnuadhiprasetyo@users.noreply.github.com>
2019-03-12 17:37:39 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<h1>Hello World</h1> <br/><table>
+    <tr>
+        <td>NIM</td>
+        <td> : </td>
+        <td>16.01.53.0086</td>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td> : </td>
+        <td>Wisnu Adhi Prasetyo</td>
+    </tr>
+</table>
+<a href="/akademik">Go To Akademik</a>
